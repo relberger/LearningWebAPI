06ae6d9e70a3cbf77aa4d1c5c3c383fdc2988b73
relberger <relberger96@gmail.com>
2019-02-18 23:05:49 -0500

    database set up

---
+++ todoapi/Controllers/TodoController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
+        {
+            _context = context;
+
+            if (context.TodoItems.Count() == 0)
+            {
+                //create new to do collection if empty, meaning can't delete all to do items
+
+                _context.TodoItems.Add(new TodoItem { Name = "item1" });
+                _context.SaveChanges();
+            }
+        }
+
+    //    original code template 
+    //    // GET: api/<controller>
+    //    [HttpGet]
+    //    public IEnumerable<string> Get()
+    //    {
+    //        return new string[] { "value1", "value2" };
+    //    }
+
+    //    // GET api/<controller>/5
+    //    [HttpGet("{id}")]
+    //    public string Get(int id)
+    //    {
+    //        return "value";
+    //    }
+
+    //    // POST api/<controller>
+    //    [HttpPost]
+    //    public void Post([FromBody]string value)
+    //    {
+    //    }
+
+    //    // PUT api/<controller>/5
+    //    [HttpPut("{id}")]
+    //    public void Put(int id, [FromBody]string value)
+    //    {
+    //    }
+
+    //    // DELETE api/<controller>/5
+    //    [HttpDelete("{id}")]
+    //    public void Delete(int id)
+    //    {
+    //    }
+    //}
+}
